6253af502678d226a6961888e5dbe2d057b7d2ad
Apk_boi <abraham.nnu@gmail.com>
2022-06-26 14:21:53 +0100

    edit post

--- lib/screens/main/feed/widgets/edit_post_form.dart
+++ lib/screens/main/feed/widgets/edit_post_form.dart
@@ -9,7 +9,8 @@ import '../../../widget/custom_button.dart';
 import 'image_picker_item.dart';
 
 class EditPostForm extends StatefulWidget {
-  const EditPostForm({Key? key, required this.feed, required this.onSucces}) : super(key: key);
+  const EditPostForm({Key? key, required this.feed, required this.onSucces})
+      : super(key: key);
   final Feed feed;
   final VoidCallback onSucces;
 
@@ -26,7 +27,7 @@ class _EditPostFormState extends State<EditPostForm> {
   @override
   void initState() {
     _contentController.text =
-    widget.feed.content == null ? '' : widget.feed.content!;
+        widget.feed.content == null ? '' : widget.feed.content!;
     super.initState();
   }
 
@@ -38,17 +39,14 @@ class _EditPostFormState extends State<EditPostForm> {
       bloc: _feedBloc,
       listener: (context, state) {
         if (state is EditFeedLoadingState) {
-          AppUtils.showAnimatedProgressDialog(
-              context,
+          AppUtils.showAnimatedProgressDialog(context,
               title: "Updating, please wait...");
         }
         if (state is EditFeedSuccessState) {
           widget.onSucces();
           Navigator.of(context).pop();
           // AppUtils.cancelAllShowingToasts();
-          AppUtils.showCustomToast(
-              "Post has been updated successfully");
-
+          AppUtils.showCustomToast("Post has been updated successfully");
         }
         if (state is EditFeedFaliureState) {
           Navigator.of(context).pop();
@@ -56,26 +54,22 @@ class _EditPostFormState extends State<EditPostForm> {
         }
       },
       child: Container(
-
         child: Container(
           padding: const EdgeInsets.all(30),
           decoration: BoxDecoration(
-              color: Theme
-                  .of(context)
-                  .cardColor,
+              color: Theme.of(context).cardColor,
               borderRadius: const BorderRadius.only(
                   topLeft: Radius.circular(30), topRight: Radius.circular(30))),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
             children: [
-
               Center(
                   child: Container(
-                    color: Colors.grey,
-                    width: 100,
-                    height: 2.5,
-                  )),
+                color: Colors.grey,
+                width: 100,
+                height: 2.5,
+              )),
               const SizedBox(
                 height: 15,
               ),
@@ -88,11 +82,7 @@ class _EditPostFormState extends State<EditPostForm> {
               ),
               Padding(
                 padding: EdgeInsets.only(
-
-                    bottom: MediaQuery
-                        .of(context)
-                        .viewInsets
-                        .bottom),
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
                 child: Form(
                   key: _fieldKey,
                   child: TextFormField(
@@ -100,11 +90,9 @@ class _EditPostFormState extends State<EditPostForm> {
                       RequiredValidator(errorText: 'Content must not be empty'),
                       // EmailValidator(errorText: 'Enter a valid email'),
                     ]),
-
                     controller: _contentController,
                     maxLines: 5,
                     minLines: 1,
-
                     decoration: const InputDecoration(
                         contentPadding: EdgeInsets.all(10),
                         focusedBorder: OutlineInputBorder(),
@@ -123,68 +111,69 @@ class _EditPostFormState extends State<EditPostForm> {
                   },
                   child: pickedImages.isNotEmpty
                       ? Column(
-                    children: [
-                      SizedBox(
-                        height: 150,
-                        child: ListView.builder(
-                            physics:
-                            const BouncingScrollPhysics(),
-                            itemCount: pickedImages.length,
-                            shrinkWrap: true,
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (context, index) =>
-                                ImagePickerItem(
-                                  image: pickedImages[index],
-                                  onClose: () {
-                                    setState(() {
-                                      pickedImages.remove(
-                                          pickedImages[index]);
-                                      // mediaFiles.removeAt(index);
-                                    });
-                                  },
-                                )),
-                      ),
-                      const Text(
-                        'Tap to add image',
-                        style: TextStyle(color: Colors.blueGrey),
-                      )
-                    ],
-                  )
+                          children: [
+                            SizedBox(
+                              height: 150,
+                              child: ListView.builder(
+                                  physics: const BouncingScrollPhysics(),
+                                  itemCount: pickedImages.length,
+                                  shrinkWrap: true,
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder: (context, index) =>
+                                      ImagePickerItem(
+                                        image: pickedImages[index],
+                                        onClose: () {
+                                          setState(() {
+                                            pickedImages
+                                                .remove(pickedImages[index]);
+                                            // mediaFiles.removeAt(index);
+                                          });
+                                        },
+                                      )),
+                            ),
+                            const Text(
+                              'Tap to add image',
+                              style: TextStyle(color: Colors.blueGrey),
+                            )
+                          ],
+                        )
                       : Container(
-                    width: MediaQuery.of(context).size.width,
-                    padding: const EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        color: Theme.of(context).cardColor),
-                    child: Center(
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            'assets/pngs/upload_image.png',
-                            height: 200,
+                          width: MediaQuery.of(context).size.width,
+                          padding: const EdgeInsets.all(16.0),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.0),
+                              color: Theme.of(context).cardColor),
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  'assets/pngs/upload_image.png',
+                                  height: 200,
+                                ),
+                                const Text(
+                                  'Tap To Upload Image',
+                                  style: TextStyle(
+                                      color: Colors.blueGrey, fontSize: 16),
+                                )
+                              ],
+                            ),
                           ),
-                          const Text(
-                            'Tap To Upload Image',
-                            style: TextStyle(
-                                color: Colors.blueGrey,
-                                fontSize: 16),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
+                        ),
                 ),
               ),
-
-              SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               CustomButton(
                 onTap: () {
                   if (_fieldKey.currentState!.validate()) {
                     _feedBloc.add(EditFeedEvent(
                         feed_id: widget.feed.id.toString(),
                         content: _contentController.text,
-                        pageId: widget.feed.type == 'page_feed' ? widget.feed
-                            .pageId : null));
+                        pageId: widget.feed.type == 'page_feed'
+                            ? widget.feed.pageId
+                            : null));
+
                     // _profileBloc.add(UpdateProfileEvent(phone: _phoneNumberController.text));
                     // _authBloc.add(ForgotPasswordEvent(email: _phoneNumberController.text));
 
@@ -198,6 +187,7 @@ class _EditPostFormState extends State<EditPostForm> {
       ),
     );
   }
+
   void _fetchMedia() async {
     var images = ['jpg', 'jpeg', 'png', 'webp', 'PNG'];
     // var videos = ['mp4', 'mov'];
@@ -232,5 +222,4 @@ class _EditPostFormState extends State<EditPostForm> {
           }
         });
   }
-
 }
