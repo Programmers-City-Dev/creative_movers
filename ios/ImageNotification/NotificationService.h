5e01c522ae08187dbd0d2c592d82dc7888604392
zubisofts <zubitex40@gmail.com>
2023-03-07 07:14:14 +0100

    Major app fixes and cleanup

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Anyanwu Nzubechi on 06/03/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
